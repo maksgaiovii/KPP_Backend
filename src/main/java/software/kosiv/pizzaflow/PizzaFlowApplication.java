604841db53d60a01a3454d048659824699c535fe
superden2256 <denys200517@gmail.com>
2024-11-04 22:45:08 +0200

    project initialization

---
+++ src/main/java/software/kosiv/pizzaflow/PizzaFlowApplication.java
@@ -0,0 +1,13 @@
+package software.kosiv.pizzaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaFlowApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaFlowApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/software/kosiv/pizzaflow/PizzaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package software.kosiv.pizzaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaFlowApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
